4e3a9898a248b669f50927537318cd81a368f145
yyechan0602 <yyechan0602@naver.com>
2023-08-16 15:47:19 +0900

    add logo

--- src/layouts/full/shared/logo/Logo.js
+++ src/layouts/full/shared/logo/Logo.js
@@ -1,20 +1,24 @@
-import { Link } from 'react-router-dom';
-import { ReactComponent as LogoDark } from 'src/assets/images/logos/dark-logo.svg';
-import { styled } from '@mui/material';
+import {Link} from 'react-router-dom';/*
+import { ReactComponent as LogoDark } from 'src/assets/images/logos/work.svg';
+import { ReactComponent as logo2 } from 'src/assets/images/logos/work.svg';*/
+import logo from 'src/assets/images/logos/work.png';
+import {styled} from '@mui/material';
 
 const LinkStyled = styled(Link)(() => ({
-  height: '70px',
-  width: '180px',
-  overflow: 'hidden',
-  display: 'block',
+    height: '70px',
+    width: '300px',
+    overflow: 'hidden',
+    display: 'block',
 }));
 
 const Logo = () => {
-  return (
-    <LinkStyled to="/">
-      <LogoDark height={70} />
-    </LinkStyled>
-  )
+    return (
+        <LinkStyled to="/">
+          <img src={logo} alt="main logo" className="main-logo" height="100%"/>
+        </LinkStyled>
+    )
 };
 
 export default Logo;
+
+/*<LogoDark height={70}*/
